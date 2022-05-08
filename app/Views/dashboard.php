b5d2fc6a490a40749e718f7e506a23319da0c60c
Tegarsw17 <tegarsatriyawiguna99@gmail.com>
2022-05-08 22:16:05 +0700

    update view

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -189,7 +189,7 @@
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
                                                 Jenis Pohon</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $jenis_pohon[0]['jenis']; ?></div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
                                         </div>
                                         <div class="col-auto">
                                             <!-- <i class="fas fa-comments fa-2x text-gray-500"></i> -->
